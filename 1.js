f8407c4f682b2ff722528b5b961263a1352351ab
Rakesh G <rakesh.g@drinkprime.in>
2023-09-28 11:57:21 +0530

    testing1

--- 1.js
+++ 1.js
@@ -2,9 +2,10 @@
 //     console.log(x,y,z);
 //     return x+y+z
 // }
-import {person} from "./2.js";
+import { person } from "./2.js";
 
-person 
-console.log(`imported value of ${person} from 2.js`)
+console.log(`imported value of ${person} from 2.js`);
 
-console.log("just consoled")
+console.log("just consoled");
+
+export const object = { x: person, y: 1, z: 3 };
